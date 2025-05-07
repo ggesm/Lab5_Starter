aacd88b3c0a6620186eacaedec75460c56b8d8b8
ggesm <117891063+ggesm@users.noreply.github.com>
2025-05-07 00:45:59 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,7 +7,7 @@ function init() {
   // const volumeImage = document.querySelector("#volume-controls img");
   const hornImage = document.querySelector("#expose img");
   // const button = document.querySelector("button");
-  // const volumeSlider = document.getElementId('volume');
+  // const volumeSlider = document.getElementById('volume');
   const audio = document.querySelector("audio");
   // const jsConfetti = new jsConfetti();
 
