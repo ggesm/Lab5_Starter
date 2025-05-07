e226d23155c9325c9469211091ce22fe4c8f5d57
ggesm <117891063+ggesm@users.noreply.github.com>
2025-05-07 00:33:42 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -9,7 +9,7 @@ function init() {
   const button = document.querySelector("button");
   const volumeSlider = document.getElementId('volume');
   const audio = document.querySelector("audio");
-  const jsConfetti = new jsConfetti();
+  // const jsConfetti = new jsConfetti();
 
   hornSelect.addEventListener('change', () => {
     const horn = hornSelect.value;
