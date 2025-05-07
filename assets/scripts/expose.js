1a1b590a3fda5445699dc1184799ae3a27f5ab36
ggesm <117891063+ggesm@users.noreply.github.com>
2025-05-07 00:54:14 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,17 +5,15 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   const hornSelect = document.getElementById('horn-select');
   // const volumeImage = document.querySelector("#volume-controls img");
-  const hornImage = document.querySelector("#expose img");
+  const hornImage = document.querySelector("img");
   // const button = document.querySelector("button");
   // const volumeSlider = document.getElementById('volume');
   const audio = document.querySelector("audio");
   // const jsConfetti = new jsConfetti();
 
   hornSelect.addEventListener('change', () => {
-    const horn = hornSelect.value;
-
-    hornImage.src = `assets/images/${horn}.svg`;
-
-    audio.src = `assets/audio/${horn}.mp3`;
+    const hornInfo = hornSelect.value;
+    hornImage.src = `assets/images/${hornInfo}.svg`;
+    audio.src = `assets/audio/${hornInfo}.mp3`;
   });
 }
