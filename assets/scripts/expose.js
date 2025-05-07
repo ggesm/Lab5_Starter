10312543467103025fce274115be7f8810e8fb76
ggesm <117891063+ggesm@users.noreply.github.com>
2025-05-06 23:03:55 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img[alt="No image selected"]');
+  const audio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const playButon = document.querySelector('button');
+  const jsConfetti = new jsConfetti();
+
+  hornSelect.addEventListener('change', () => {
+    const horn = hornSelect.value
+    hornTmage.src = `assets/images/${horn}.svg`
+    audio.src = `assets/audio/${horn}.mp3`
+  });
+
+  volumeSlider.addEventListener('input', {} => {
+    const volume = volumeSlider.value;
+    audio.volume = volume/100;
+  });
+
+  playButton.addEventListener('click', () => {
+    audio.play();
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
