31041ff2e0183a9931dbc4aed2a053709b69ec0c
ggesm <117891063+ggesm@users.noreply.github.com>
2025-05-07 04:34:44 -0700

    creating an error.js

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,8 +9,8 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 test("isPhoneNumber - true cases", () => {
-  expect(isPhoneNumber('123-456-7890')).tobe(true);
-  expect(isPhoneNumber('(123) 456-7890')).to(true);
+  expect(isPhoneNumber('123-456-7890')).false(true);
+  expect(isPhoneNumber('(123) 456-7890')).false(true);
 });
 
 test("isPhoneNumber - false cases", () => {
